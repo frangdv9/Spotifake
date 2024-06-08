160ac755198295251e4a2720c76fbc96dc80b479
LiontailSolution <liontailsolutions@gmail.com>
2024-06-08 16:01:53 -0300

    create Genre Enum

---
+++ src/main/java/com/davinci/spotifake/Model/Genre.java
@@ -0,0 +1,24 @@
+package com.davinci.spotifake.Model;
+
+public enum Genre{
+    ROCK,
+    POP,
+    JAZZ,
+    HIP_HOP,
+    ELECTRÓNICA,
+    REGGAE,
+    CLÁSICA,
+    RKT,
+    COUNTRY,
+    BLUES,
+    METAL,
+    INDIE,
+    FOLK,
+    REGGAETON,
+    SOUL,
+    PUNK,
+    EDM,
+    FUNK,
+    TRAP,
+    ALTERNATIVO
+}
