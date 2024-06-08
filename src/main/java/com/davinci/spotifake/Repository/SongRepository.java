11b98568230bdff7f82d94722a0b7a9032df91ae
LiontailSolution <liontailsolutions@gmail.com>
2024-06-08 16:15:09 -0300

    create SongRespository

---
+++ src/main/java/com/davinci/spotifake/Repository/SongRepository.java
@@ -0,0 +1,11 @@
+package com.davinci.spotifake.Repository;
+
+import com.davinci.spotifake.Model.Song;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, Long> {
+
+
+}
