8d9335ff22cd04a5f25456aae95bc4a0e4c71cf9
LiontailSolution <liontailsolutions@gmail.com>
2024-06-15 16:46:46 -0300

    feat ArtistDiskService

---
+++ src/main/java/com/davinci/spotifake/Service/ArtistDiskService.java
@@ -0,0 +1,31 @@
+package com.davinci.spotifake.Service;
+
+import com.davinci.spotifake.Model.ArtistDisk;
+import com.davinci.spotifake.Repository.ArtistDiskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ArtistDiskService {
+
+    private final ArtistDiskRepository artistDiskRepository;
+
+    @Autowired
+    public ArtistDiskService(ArtistDiskRepository artistDiskRepository) {
+        this.artistDiskRepository = artistDiskRepository;
+    }
+
+
+    public List<ArtistDisk> getAllArtistDisks() {
+        return artistDiskRepository.findAll();
+    }
+
+
+    public ArtistDisk saveArtistDisk(ArtistDisk artistDisk) {
+        return artistDiskRepository.save(artistDisk);
+    }
+
+
+}
