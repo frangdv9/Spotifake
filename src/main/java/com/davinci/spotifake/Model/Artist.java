236a431c8063a7f6ea2b0983a0bca199cb7bc3d7
LiontailSolution <liontailsolutions@gmail.com>
2024-06-15 15:43:25 -0300

    feat Artist Model

--- src/main/java/com/davinci/spotifake/Model/Artist.java
+++ src/main/java/com/davinci/spotifake/Model/Artist.java
@@ -19,20 +19,24 @@ public class Artist {
 
     private Date birthDate;
 
+    private Date deathDate;
+
     private Instrument instrument;
 
     private String biography;
     @OneToMany(mappedBy = "artist", cascade = CascadeType.ALL)
+
     private List<Disk> disks;
     public Artist() {
 
     }
 
-    public Artist(Long id, Genre genre, Nationality nationality, Date birthDate, Instrument instrument, String biography, List<Disk> disks) {
+    public Artist(Long id, Genre genre, Nationality nationality, Date birthDate, Date deathDate, Instrument instrument, String biography, List<Disk> disks) {
         this.id = id;
         this.genre = genre;
         this.nationality = nationality;
         this.birthDate = birthDate;
+        this.deathDate = deathDate;
         this.instrument = instrument;
         this.biography = biography;
         this.disks = disks;
@@ -92,5 +96,12 @@ public class Artist {
 
     public void setDisks(List<Disk> disks) {
         this.disks = disks;
+    }
+
+    public Date getDeathDate() {
+        return deathDate;
+    }
+    public void setDeathDate(Date deathDate) {
+        this.deathDate = deathDate;
     }
 }
